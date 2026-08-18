d9460e9de5a4f20e003de7b06be298709aa38bba
kimsj8002-ux <kimsj8002@gmail.com>
2026-08-19 08:13:32 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "jibangdo-v2";
+const CACHE = "jibangdo-v5";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {
